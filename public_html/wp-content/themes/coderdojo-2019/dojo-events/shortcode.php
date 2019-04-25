b892a4d8bc3af7a45bc93e553f2efc624b54215c
Tim Brugman <tim@timbrugman.com>
2019-04-25 22:14:45 +0200

    Switched back to the non-Gutenberg code.

--- public_html/wp-content/themes/coderdojo-2019/dojo-events/shortcode.php
+++ public_html/wp-content/themes/coderdojo-2019/dojo-events/shortcode.php
@@ -2,13 +2,13 @@
 // Exit if accessed directly
 if ( ! defined( 'ABSPATH' ) ) exit;
 
-// add_shortcode( 'dojoevent', function(){
-// 	coderdojo_output_event();
-// });
+add_shortcode( 'dojoevent', function(){
+	coderdojo_output_event();
+});
 
 // gutenberg ready?
-add_shortcode( 'dojoevent', function(){
-    ob_start();
-    coderdojo_output_event();
-    return ob_get_clean();
-});
+// add_shortcode( 'dojoevent', function(){
+//     ob_start();
+//     coderdojo_output_event();
+//     return ob_get_clean();
+// });
